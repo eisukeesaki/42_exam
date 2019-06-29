fc982676dc420f9204da692822a3462da0223ea8
Eisuke Esaki <eesaki@student.42.us.org>
2019-06-28 22:51:52 -0700

    aff_z

---
+++ level0/aff_z/aff_z.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_z.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eesaki <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/28 21:10:09 by eesaki            #+#    #+#             */
+/*   Updated: 2019/06/28 22:49:17 by eesaki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	aff_z(char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == 'z')
+		{
+			ft_putchar(s[i]);
+			break ;
+		}
+		i++;
+	}
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 2)
+		aff_a(av[1]);
+	else
+		ft_putchar('z');
+	ft_putchar('\n');
+	return (0);
+}
