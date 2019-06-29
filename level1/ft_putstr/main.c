5ba09b073f6a77db258987eacfaa0a9bc5764bc1
eisuke <izkyyy@gmail.com>
2019-06-29 02:53:45 -0700

    ft_putstr

---
+++ level1/ft_putstr/main.c
@@ -0,0 +1,15 @@
+int		main(void)
+{
+	char	s0[] = "abcdefg";
+	ft_putstr(s0);
+	write(1, "\n", 1);
+
+	char	s1[] = "";
+	ft_putstr(s1);
+	write(1, "\n", 1);
+
+	char	s2[] = "abcdefg 123456789";
+	ft_putstr(s2);
+
+	return (0);
+}
